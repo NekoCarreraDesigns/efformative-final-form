5c82358be2bb220d9e265ddc2711d5fbc3bb2cb2
NekoCarreraDesigns <denvermaas@gmail.com>
2024-03-01 15:28:22 -0700

    added code to files and removed react import for final form

---
+++ eformative-final-form/src/components/LogoHeader/Logo.jsx
@@ -0,0 +1,12 @@
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <h1 className='page-header-logo'>
+      <img alt='logo' className='logo' src='/assets/eLogo.jpg'></img>
+      formative
+    </h1>
+  );
+};
+
+export default Logo;
