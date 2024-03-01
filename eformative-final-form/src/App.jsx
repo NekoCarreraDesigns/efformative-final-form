2454f7e3ec24786bfef5fd44b7f237a95d9f39a6
NekoCarreraDesigns <denvermaas@gmail.com>
2024-03-01 14:52:53 -0700

    added code for app.jsx and deleted default image

--- eformative-final-form/src/App.jsx
+++ eformative-final-form/src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import Logo from "./components/LogoHeader/Logo";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Market from "./pages/Market/Market";
+import Reviews from "./pages/Reviews/Reviews";
+import Sell from "./pages/Sell/Sell";
+import Signup from "./pages/Signup/Signup";
+import Seller from "./pages/Seller/Seller";
+import PostItem from "./pages/Post-Item/PostItem";
+import PostReview from "./pages/Post-Review/PostReview";
+import SellerReviews from "./pages/Seller-Reviews/SellerReviews";
+import ProductReview from "./pages/Product-Reviews/ProductReview";
+import Footer from "./components/Footer/Footer";
+import About from "./pages/About/About";
+import AdminRegistration from "./pages/Admin/adminRegistration";
+import AdminLogin from "./pages/Admin/adminLogin";
+import Admin from "./pages/Admin/Admin";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Logo />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/market' element={<Market />}></Route>
+        <Route path='/reviews' element={<Reviews />}></Route>
+        <Route path='/sell' element={<Sell />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/seller' element={<Seller />}></Route>
+        <Route path='/post-item' element={<PostItem />}></Route>
+        <Route path='/post-reviews' element={<PostReview />}></Route>
+        <Route path='/seller-reviews' element={<SellerReviews />}></Route>
+        <Route path='/product-reviews' element={<ProductReview />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route
+          path='/admin-registration'
+          element={<AdminRegistration />}></Route>
+        <Route path='/admin-login' element={<AdminLogin />}></Route>
+        <Route path='/admin' element={<Admin />}></Route>
+      </Routes>
+      <Footer />
     </>
   )
 }
