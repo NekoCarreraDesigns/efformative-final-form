1bbe4db0762d0b58794ac9413789c69ca0d622c0
NekoCarreraDesigns <denvermaas@gmail.com>
2024-03-01 15:55:29 -0700

    renamed files and removed react imports

---
+++ eformative-final-form/src/pages/About/About.jsx
@@ -0,0 +1,22 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <>
+      <div className="home-hero">
+        <h1 className='about-page-header'>About Us</h1>
+        <p className='about-page-paragraph'>
+          Eformative site created by Quality Networks.
+          <br />
+          The site was created as an alternative marketplace for electric
+          transportation alternatives to create a greener environment for all of
+          us to live in. It is our belief that we can make the world a better
+          place!
+
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
